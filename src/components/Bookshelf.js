92c2599cb661ba263e9975bc7a10df98ecce5df4
Sareeta054 <kunwarsareeta12@gmail.com>
2020-06-16 13:37:59 +0545

    Used api to get books and  also updated books

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
 export default class Bookshelf extends Component {
+    static propTypes ={
+        booksOnShelf: PropTypes.array.isRequired,
+        changeShelf: PropTypes.func.isRequired,
+    }
+
     render() {
+        const shelves = ['currentlyReading', 'wantToRead', 'read']
+        const shelveNames = ['Currently Reading', 'Want To Read', 'Read']
+
         return (
             <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book />
-                      </ol>
-                  </div>
-                </div>
+                {shelves.map((shelf, index) => {
+                    return(
+                        <div key={index} className='list-books-content'>
+                            <div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">{shelveNames[index]}</h2>
+                                    <div className="bookshelf-books">
+                                        <ol className="books-grid">
+                                            {this.props.booksOnShelf.sort().filter(book => book.shelf === shelf)
+                                            .map(book => (
+                                                <Book key={book.id}
+                                                book={book}
+                                                changeShelf={this.props.changeShelf} />
+                                            ))}
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )})}
             </div>
         )
     }
