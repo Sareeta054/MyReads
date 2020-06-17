4221f7abc614cff5f57e4f70e674b4fb8aa75792
Sareeta054 <kunwarsareeta12@gmail.com>
2020-06-17 09:00:30 +0545

    made it possible to search books

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import Book from './Book'
 import * as BooksAPI from '../utils/BooksAPI'
+import PropTypes from 'prop-types'
 
 export default class SearchPage extends Component {
+  state = {
+    query: '',
+    books: []
+  }
+
+  updateQuery = query => {
+    this.setState({query: query.trim()})
+    BooksAPI.search(query, 30).then(books => {this.setState({books})})
+  }
     render() {
         return (  
           <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search"  to='/'> Close </Link>
                 <div className="search-books-input-wrapper">
-                  <input type="text" placeholder="Search by title or author"/>
+                  <input type="text" placeholder="Search by title or author"
+                  onChange={e => this.updateQuery(e.target.value)} />
                 </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                {this.state.books.sort().map(book => (
+                  <Book changeShelf={this.props.changeShelf}
+                  key={book.id} book={book} />
+                ))}
+              </ol>
             </div>
           </div>
         )
